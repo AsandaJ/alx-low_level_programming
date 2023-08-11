1700b35bfa591b77d8d3239cfc02df9230e325de
AsandaJ <asandayanela0@gmail.com>
2023-08-11 13:09:52 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print digits using putchar
+ * Return: At Success 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
+
