d70ded7e77d3f451819e44382ea3b10e44e5b7dd
AsandaJ <asandayanela0@gmail.com>
2023-08-16 23:05:40 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* main - Evaluate function identifying positive or negative integers
+* Return: 0
+*/
+int main(void)
+{
+int j;
+j = 96;
+positive_or_negative(j);
+return (0);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,6 @@
+0x03. C - Debugging
+0. Multiple mains
+Like, comment, subscribe
+2. 0 > 972?
+3. Leap year
+
